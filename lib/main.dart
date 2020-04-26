14043c74b1ef8d85add9685ac5a1949a307262c9
H7amady <medvallhamady@gmail.com>
2020-04-26 14:32:59 +0000

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: AssetImage('assets/images/hamady.jpg'),
+                  ),
+                  Text(
+                    'Hamady Ahmed',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Pacifico',
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'FLUTTER DEVELOPER',
+                    style: TextStyle(
+                      color: Colors.teal.shade100,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 25.0,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 150.0,
+                    height: 20.0,
+                    child: Divider(
+                      color: Colors.teal.shade100,
+                    ),
+                  ),
+                  Card(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        '+222 37003685',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: "SourceSansPro",
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'hamadya7med@gmail.com',
+                        style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontSize: 20.0,
+                            fontFamily: 'SourceSansPro'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )),
+    );
+  }
 }
